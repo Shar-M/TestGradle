944522d289a5b928119a2c9adfae77ac75991ffc
kowlalis <sharanyakowlali@gmail.com>
2017-06-06 14:57:42 +0530

    first commit

---
+++ src/main/java/OddEven.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Created by kowlalis on 6/6/17.
+ */
+public class OddEven {
+    public String checkIfOddOrEven(int number) {
+        if(number%2==0)
+            return "Even";
+        else
+            return "Odd";
+    }
+
+
+}
---
+++ src/test/java/OddEvenTest.java
@@ -0,0 +1,15 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by kowlalis on 6/6/17.
+ */
+public class OddEvenTest {
+    @Test
+    public void testOdd(){
+        OddEven oddEven = new OddEven();
+        assertEquals("Odd",oddEven.checkIfOddOrEven(5));
+    }
+}
